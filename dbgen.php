95f35a7a43ef1fd0187599b5d53b9a5b70f6a9d2
metalg091 <tibi.furesz@gmail.com>
2023-02-12 05:48:37 +0100

    Dynamic table creation
    Now each new wordlist will have its own table with an automatically assigned id

--- dbgen.php
+++ dbgen.php
@@ -15,32 +15,39 @@
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
-    $sql = "CREATE TABLE `0` (
+    $sql = "SELECT id FROM `ite`";
+    $result = mysqli_query($conn, $sql);
+    $row   = mysqli_fetch_row($result); //row[0] (single variable)
+    $sql = "CREATE TABLE `" . $row[0] . "` (
         `id` INT NOT NULL UNIQUE, 
         `fore` TINYTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL, 
         `nat` TINYTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL, 
         PRIMARY KEY (`id`)
         )";
-
     if (mysqli_query($conn, $sql)) {
-        echo "Table MyGuests created successfully";
+        echo "Table " . $row[0] . " created successfully<br>";
     } else {
-        echo "Error creating table: " . mysqli_error($conn);
+        echo "<br>Error creating table: " . mysqli_error($conn);
     }
     $id = 0;
     for($i = 0; $i < count($inp)-1; $i+=2){
         $inp[$i] = mysqli_real_escape_string($conn, $inp[$i]);
         $inp[$i+1] = mysqli_real_escape_string($conn, $inp[$i+1]);
-        $sql = "INSERT INTO `0` (`id`, `fore`, `nat`) VALUES ('" . $id . "', '" . $inp[$i] . "', '" . $inp[$i+1] . "');";
+        $sql = "INSERT INTO `" . $row[0] . "` (`id`, `fore`, `nat`) VALUES ('" . $id . "', '" . $inp[$i] . "', '" . $inp[$i+1] . "');";
         //Don't worry phpMyAdmin does NOT disply it as utf8 and thus the input seems corrupted, but it is NOT!!!
         if (mysqli_query($conn, $sql)) {
-            echo "Inserted data successfully";
+            echo "Inserted data successfully<br>";
         } else {
-            echo "Error: ". $sql . "<br>" . mysqli_error($conn);
+            echo "<br>Error: ". $sql . "<br>" . mysqli_error($conn);
         }
         $id++;
     }
-
-    echo '<pre>'; print_r($inp); echo '</pre>';
+    $sql = "UPDATE `ite` SET id='" . $row[0] + 1 . "' WHERE id=" . $row[0];
+    if (mysqli_query($conn, $sql)) {
+        echo "Record updated successfully<br>";
+    } else {
+        echo "<br>Error updating record: " . mysqli_error($conn);
+    }
+    //echo '<pre>'; print_r($inp); echo '</pre>';
     mysqli_close($conn);
 ?>
